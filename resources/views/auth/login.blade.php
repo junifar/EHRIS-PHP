9ccebff2173e964b7d353f5144647f1814b9fbaf
junifar <junifar@gmail.com>
2017-07-23 09:03:15 +0700

    Login Screen Custom

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,107 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
+
+
+    <!-- CSS Files -->
+    <link href="{{ asset('component/now-ui-kit/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('component/now-ui-kit/assets/css/now-ui-kit.css') }}" rel="stylesheet">
+
+    <!-- CSS Just for demo purpose, don't include it in your project -->
+    <link href="{{ asset('component/now-ui-kit/assets/css/demo.css') }}" rel="stylesheet">
+</head>
+<body class="login-page">
+    <!-- End Navbar -->
+    <div class="page-header" filter-color="orange">
+        <div class="page-header-image" style="background-image:url({{ asset('component/now-ui-kit/assets/img/login.jpg') }})"></div>
+        <div class="container">
+            <div class="col-md-4 content-center">
+                <div class="card card-login card-plain">
+                    <form class="form" method="" action="">
+                        <div class="header header-primary text-center">
+                            <div class="logo-container">
+                                <img src="{{ asset('component/now-ui-kit/assets/img/now-logo.png')  }}">
+                            </div>
+                        </div>
+                        <div class="content">
+                            <div class="input-group form-group-no-border input-lg">
+                                    <span class="input-group-addon">
+                                        <i class="now-ui-icons users_circle-08"></i>
                                     </span>
-                                @endif
+                                <input type="text" class="form-control" placeholder="First Name...">
                             </div>
-                        </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
+                            <div class="input-group form-group-no-border input-lg">
+                                    <span class="input-group-addon">
+                                        <i class="now-ui-icons ui-1_lock-circle-open"></i>
                                     </span>
-                                @endif
+                                <input type="password" placeholder="Password..." class="form-control" />
                             </div>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
+                        <div class="footer text-center">
+                            <a href="#pablo" class="btn btn-primary btn-round btn-lg btn-block">Get Started</a>
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
+                        <div class="pull-left">
+                            <h6>
+                                <a href="#pablo" class="link">Create Account</a>
+                            </h6>
+                        </div>
+                        <div class="pull-right">
+                            <h6>
+                                <a href="#pablo" class="link">Need Help?</a>
+                            </h6>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+        <footer class="footer">
+            <div class="container">
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="http://presentation.creative-tim.com">
+                                About Us
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://prasetia.co.id">
+                                website
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="copyright">
+                    &copy;
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>, Designed by
+                    <a href="http://www.invisionapp.com" target="_blank">Invision</a>. Coded by
+                    <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+                </div>
+            </div>
+        </footer>
     </div>
-</div>
-@endsection
+</body>
+<script src="{{ asset('component/now-ui-kit/assets/js/core/jquery.3.2.1.min.js')}}"></script>
+<script src="{{ asset('component/now-ui-kit/assets/js/core/tether.min.js')}}"></script>
+<script src="{{ asset('component/now-ui-kit/assets/js/core/bootstrap.min.js')}}"></script>
+<script src="{{ asset('component/now-ui-kit/assets/js/plugins/bootstrap-switch.js')}}"></script>
+
+<script src="{{ asset('component/now-ui-kit/assets/js/plugins/nouislider.min.js')}}"></script>
+<script src="{{ asset('component/now-ui-kit/assets/js/plugins/bootstrap-datepicker.js')}}"></script>
+<script src="{{ asset('component/now-ui-kit/assets/js/now-ui-kit.js')}}"></script>
+</html>
