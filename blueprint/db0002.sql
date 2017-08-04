f03a563efb1bc79223764321048a7db0b6f7ece0
junifar <junifar@gmail.com>
2017-08-04 18:02:40 +0700

    Adding models and some master table

---
+++ blueprint/db0002.sql
@@ -0,0 +1,237 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 192.168.1.155
+Source Server Version : 50516
+Source Host           : 192.168.1.155:3306
+Source Database       : db0002
+
+Target Server Type    : MYSQL
+Target Server Version : 50516
+File Encoding         : 65001
+
+Date: 2017-08-04 18:02:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for banks
+-- ----------------------------
+DROP TABLE IF EXISTS `banks`;
+CREATE TABLE `banks` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for companies
+-- ----------------------------
+DROP TABLE IF EXISTS `companies`;
+CREATE TABLE `companies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `USER_CREATED` int(255) DEFAULT NULL,
+  `USER_MODIFIED` int(255) DEFAULT NULL,
+  `COMPANY_STATUS_ID` int(11) DEFAULT NULL,
+  `ADDRESS` text,
+  `PHONE` varchar(255) DEFAULT NULL,
+  `LOGO` blob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2949064 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for company_statuses
+-- ----------------------------
+DROP TABLE IF EXISTS `company_statuses`;
+CREATE TABLE `company_statuses` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for departments
+-- ----------------------------
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  `COMPANY_ID` int(11) DEFAULT NULL,
+  `MANAGER_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for employees
+-- ----------------------------
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE `employees` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `COMPANY_ID` int(11) DEFAULT NULL,
+  `NAME` varchar(255) DEFAULT NULL,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `CREATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  `NOREG` varchar(20) DEFAULT NULL,
+  `NO_KTP` varchar(20) DEFAULT NULL,
+  `RELIGION_ID` int(11) DEFAULT NULL,
+  `PHONE` varchar(255) DEFAULT NULL,
+  `HANDPHONE` varchar(255) DEFAULT NULL,
+  `TEMPAT_LAHIR` varchar(255) DEFAULT NULL,
+  `TANGGAL_LAHIR` date DEFAULT NULL,
+  `GENDER_ID` int(11) DEFAULT NULL,
+  `STATUS_ID` int(11) DEFAULT NULL,
+  `EMPLOYEE_STATUS_ID` int(11) DEFAULT NULL,
+  `REMARK` text,
+  `DEPARTMENT_ID` int(11) DEFAULT NULL,
+  `JOB_ID` int(11) DEFAULT NULL,
+  `DATE_JOINING` date DEFAULT NULL,
+  `ADDRESS` text,
+  `MANAGER_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for employee_bank_accounts
+-- ----------------------------
+DROP TABLE IF EXISTS `employee_bank_accounts`;
+CREATE TABLE `employee_bank_accounts` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ACCOUNT_NUMBER` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  `BANK_ID` int(11) DEFAULT NULL,
+  `BRANCH` varchar(255) DEFAULT NULL,
+  `NAME` varchar(255) DEFAULT NULL,
+  `REMARK` text,
+  `DEFAULTS` int(11) DEFAULT NULL,
+  `EMPLOYEE_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for employee_statuses
+-- ----------------------------
+DROP TABLE IF EXISTS `employee_statuses`;
+CREATE TABLE `employee_statuses` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for genders
+-- ----------------------------
+DROP TABLE IF EXISTS `genders`;
+CREATE TABLE `genders` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for job_titles
+-- ----------------------------
+DROP TABLE IF EXISTS `job_titles`;
+CREATE TABLE `job_titles` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for password_resets
+-- ----------------------------
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- ----------------------------
+-- Table structure for religions
+-- ----------------------------
+DROP TABLE IF EXISTS `religions`;
+CREATE TABLE `religions` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for statuses
+-- ----------------------------
+DROP TABLE IF EXISTS `statuses`;
+CREATE TABLE `statuses` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) DEFAULT NULL,
+  `CREATED_AT` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `UPDATED_AT` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `USER_CREATED` int(11) DEFAULT NULL,
+  `USER_MODIFIED` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
