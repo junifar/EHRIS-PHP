f746ff76f3dbfce7c99a28176b7b91c8179cd5ed
junifar <junifar@gmail.com>
2017-08-04 16:45:05 +0700

    Adding models and some master table

---
+++ app/DataTables/CompaniesDataTable.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Company;
+use App\User;
+use Yajra\Datatables\Services\DataTable;
+
+class CompaniesDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @return \Yajra\Datatables\Engines\BaseEngine
+     */
+    public function dataTable()
+    {
+        return $this->datatables
+            ->eloquent($this->query())
+            ->addColumn('action', '<a href="/#" class="btn btn-primary">Html Content</a>');
+    }
+
+    /**
+     * Get the query object to be processed by dataTables.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder|\Illuminate\Support\Collection
+     */
+    public function query()
+    {
+        $query = Company::query()->select($this->getColumns());
+//            ->addColumn('href', '<a href="#">Html Content</a>')
+//            ->rawColumns(['href'])
+//            ->make(true);
+
+        return $this->applyScopes($query);
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\Datatables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->columns($this->getColumns())
+                    ->minifiedAjax('')
+                    ->addAction(['width' => '80px'])
+                    ->parameters([
+                        'dom'     => 'Bfrtip',
+                        'order'   => [[0, 'desc']],
+                        'buttons' => [
+                            'create',
+                            'export',
+                            'print',
+                            'reset',
+                            'reload',
+                        ],
+                    ]);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            'id',
+            'name',
+            'created_at',
+            'updated_at'
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'companiesdatatable_' . time();
+    }
+}
---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Employee;
+use Illuminate\Http\Request;
+use Yajra\Datatables\Datatables;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('employee.index');
+    }
+
+    public function index_data(){
+        return Datatables::of(Employee::take(10000)->orderBy('id', 'DESC'))->make(true);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('employee.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = new Employee();
+        $data['name'] = $request['name'];
+        $data->save();
+        return redirect('employees');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
